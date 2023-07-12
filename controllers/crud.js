4dc7069220e822767d6e96e304f249e57fb74d20
Santiago <santiagogrillo.teves@gmail.com>
2023-07-12 16:38:50 -0300

    Casi terminado falta retocar un par de cosas

--- controllers/crud.js
+++ controllers/crud.js
@@ -3,6 +3,8 @@ const conexion = require("../database/db");
 exports.save = (req, res) => {
   const user = req.body.user;
   const rol = req.body.rol;
+
+  // Insertar usuario en la tabla "users"
   conexion.query(
     "INSERT INTO users SET ?",
     { user: user, rol: rol },
@@ -10,7 +12,21 @@ exports.save = (req, res) => {
       if (error) {
         console.log(error);
       } else {
-        res.redirect("/");
+        // Eliminar todas las notificaciones existentes excepto las de eliminación
+        conexion.query(
+          "DELETE FROM notifications WHERE message NOT LIKE 'El usuario%'",
+          (error) => {
+            if (error) {
+              console.log(error);
+            } else {
+              // Crear notificación de creación de usuario
+              const message = `El usuario ${user} se creó correctamente`;
+              insertNotification(results.insertId, message);
+
+              res.redirect("/");
+            }
+          }
+        );
       }
     }
   );
@@ -20,15 +36,113 @@ exports.update = (req, res) => {
   const id = req.body.id;
   const user = req.body.user;
   const rol = req.body.rol;
+
+  // Obtener información del usuario antes de actualizarlo
+  conexion.query("SELECT * FROM users WHERE id=?", [id], (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const previousUser = results[0].user;
+      const previousRol = results[0].rol;
+
+      // Actualizar usuario en la tabla "users"
+      conexion.query(
+        "UPDATE users SET ? WHERE id= ?",
+        [{ user: user, rol: rol }, id],
+        (error, results) => {
+          if (error) {
+            console.log(error);
+          } else {
+            // Eliminar la notificación anterior de edición de usuario si existe
+            conexion.query(
+              "DELETE FROM notifications WHERE message LIKE ?",
+              [`El usuario ${previousUser} se modificó%`],
+              (error) => {
+                if (error) {
+                  console.log(error);
+                } else {
+                  // Eliminar todas las notificaciones existentes excepto las de eliminación
+                  conexion.query(
+                    "DELETE FROM notifications WHERE message NOT LIKE 'El usuario%'",
+                    (error) => {
+                      if (error) {
+                        console.log(error);
+                      } else {
+                        // Crear notificación de edición de usuario
+                        let message = `El usuario ${previousUser} se modificó a ${user}`;
+                        if (previousRol !== rol) {
+                          message += ` y el rol ${previousRol} se modificó a ${rol}`;
+                        }
+                        insertNotification(id, message);
+
+                        res.redirect("/");
+                      }
+                    }
+                  );
+                }
+              }
+            );
+          }
+        }
+      );
+    }
+  });
+};
+
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  // Obtener información del usuario antes de eliminarlo
+  conexion.query("SELECT * FROM users WHERE id=?", [id], (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      // Eliminar usuario de la tabla "users"
+      conexion.query("DELETE FROM users WHERE id=?", [id], (error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          // Eliminar la notificación anterior de eliminación de usuario si existe
+          conexion.query(
+            "DELETE FROM notifications WHERE message LIKE ?",
+            [`El usuario ${results[0].user} se eliminó%`],
+            (error) => {
+              if (error) {
+                console.log(error);
+              } else {
+                // Eliminar todas las notificaciones existentes excepto las de eliminación
+                conexion.query(
+                  "DELETE FROM notifications WHERE message NOT LIKE 'El usuario%'",
+                  (error) => {
+                    if (error) {
+                      console.log(error);
+                    } else {
+                      // Crear notificación de eliminación de usuario
+                      const message = `El usuario ${results[0].user} se eliminó correctamente`;
+                      insertNotification(id, message);
+
+                      res.redirect("/");
+                    }
+                  }
+                );
+              }
+            }
+          );
+        }
+      });
+    }
+  });
+};
+
+function insertNotification(userId, message) {
+  // Insertar notificación en la tabla "notifications"
   conexion.query(
-    "UPDATE users SET ? WHERE id= ?",
-    [{ user: user, rol: rol }, id],
+    "INSERT INTO notifications (userId, message) VALUES (?, ?)",
+    [userId, message],
     (error, results) => {
       if (error) {
         console.log(error);
-      } else {
-        res.redirect("/");
       }
     }
   );
-};
+}
--- router.js
+++ router.js
@@ -5,13 +5,40 @@ const ejs = require("ejs");
 const crud = require("./controllers/crud");
 
 router.get("/", (req, res) => {
-  conexion.query("SELECT * FROM users", (error, results) => {
-    if (error) {
-      throw error;
-    } else {
-      res.render("index", { results: results });
-    }
+  const getUsersQuery = "SELECT * FROM users";
+  const getNotificationsQuery = "SELECT * FROM notifications";
+
+  const getUsers = new Promise((resolve, reject) => {
+    conexion.query(getUsersQuery, (error, users) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(users);
+      }
+    });
   });
+
+  const getNotifications = new Promise((resolve, reject) => {
+    conexion.query(getNotificationsQuery, (error, notifications) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(notifications);
+      }
+    });
+  });
+
+  Promise.all([getUsers, getNotifications])
+    .then(([users, notifications]) => {
+      res.render("index", {
+        user: null,
+        results: users,
+        notifications: notifications,
+      });
+    })
+    .catch((error) => {
+      throw error;
+    });
 });
 
 router.get("/create", (req, res) => {
@@ -38,6 +65,28 @@ router.get("/delete/:id", (req, res) => {
       res.redirect("/");
     }
   });
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await User.deleteUser(id);
+
+    // Obtener todos los usuarios restantes
+    const users = await User.getAllUsers();
+
+    // Actualizar los IDs de los usuarios restantes
+    for (let i = 0; i < users.length; i++) {
+      const user = users[i];
+      await User.updateUser(user.id, { id: i + 1 });
+    }
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 router.post("/save", crud.save);
